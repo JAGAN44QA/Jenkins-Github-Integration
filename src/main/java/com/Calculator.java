a8d1ea2c29319edbd15a2074b7c95f9ccc220dc4
JAGAN44QA <itzjagan4mail@gmail.com>
2022-06-10 11:14:58 +0530

    added 2 more method

--- src/main/java/com/Calculator.java
+++ src/main/java/com/Calculator.java
@@ -13,16 +13,16 @@ public class Calculator {
 
 	}
 
-//	public int multi(int a, int b) {
-//		return a*b;
-//
-//	}
-//	
-//	public int div(int a, int b) {
-//
-//		return a/b;
-//
-//	}
+	public int multi(int a, int b) {
+		return a*b;
+
+	}
+	
+	public int div(int a, int b) {
+
+		return a/b;
+
+	}
 
 //*******************************************************************************************//
 
@@ -33,6 +33,8 @@ public class Calculator {
 		Calculator c1 = new Calculator();
 		System.out.println("Additional opertaion , ANS : "+c1.add(10,20));
 		System.out.println("Subtraction opertaion , ANS : "+c1.sub(50, 32));
+		System.out.println("DIV opertaion , ANS : "+c1.div(50, 2));
+		System.out.println("MUL opertaion , ANS : "+c1.multi(50, 329));
 		System.out.println("******************************************************");
 		System.out.println("Calculator Program....... TERMINATED !");
 
