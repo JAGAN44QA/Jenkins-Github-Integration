4c6a6fb52e6bd084a5a09c8f05169bcf80073f3f
JAGAN44QA <itzjagan4mail@gmail.com>
2022-06-10 10:30:40 +0530

    Calaculator - First Commit

---
+++ src/main/java/com/Calculator.java
@@ -0,0 +1,41 @@
+package com;
+
+public class Calculator {
+	
+	
+	public int add(int a, int b) {
+		return a+b;
+
+	}
+
+	public int sub(int a, int b) {
+		return a-b;
+
+	}
+
+//	public int multi(int a, int b) {
+//		return a*b;
+//
+//	}
+//	
+//	public int div(int a, int b) {
+//
+//		return a/b;
+//
+//	}
+
+//*******************************************************************************************//
+
+	public static void main(String[] args) {
+		
+		System.out.println("Calculator Program....... START !");
+		System.out.println("******************************************************");
+		Calculator c1 = new Calculator();
+		System.out.println("Additional opertaion , ANS : "+c1.add(10,20));
+		System.out.println("Subtraction opertaion , ANS : "+c1.sub(50, 32));
+		System.out.println("******************************************************");
+		System.out.println("Calculator Program....... TERMINATED !");
+
+	}
+
+}
